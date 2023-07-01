02aa8ca0ddb281034d6f1f892bd082cdb11cad02
Jiyoung Seo <mouse890@snu.ac.kr>
2023-07-01 23:16:06 +0900

    pwm_converter

--- src/xcorps_nav/xcorps_nav/dummy_des_publisher.py
+++ src/xcorps_nav/xcorps_nav/dummy_des_publisher.py
@@ -26,7 +26,7 @@ class Dummy_Des_Publisher(Node):
         OS_des_heading.data = 30.0
         self.OS_des_heading_pub.publish(OS_des_heading)
         OS_des_spd = Float64()
-        OS_des_spd.data = 2.0
+        OS_des_spd.data = 1.0
         self.OS_des_spd_pub.publish(OS_des_spd)
         OS_heading = Float64()
         OS_heading.data = 0.0
--- src/xcorps_nav/xcorps_nav/pwm_converter.py
+++ src/xcorps_nav/xcorps_nav/pwm_converter.py
@@ -24,21 +24,23 @@ class PWMConverter(Node):
         #     self.Kp_servo = config['Kp_servo']
         #     self.Kd_servo = config['Kd_servo']
         default_params = {
-            'servo_lim' : [60, 120],
-            'pwm_lim' : [1100, 1900],
+            'rudder_lim' : [-25, 25],
+            'rudder_pwm_lim' : [1000, 2000], # 1000: left, 2000: right
+            'thrust_pwm_lim' : [1500, 1650], # 1500: stop
             'dt' : 0.1, 
-            'Kp_thrust' : 100.0,
-            'Kd_thrust' : 100.0,
-            'Kp_servo' : 100.0,
-            'Kd_servo' : 100.0,
+            'Kp_thrust' : 150.0,
+            'Kd_thrust' : 50.0,
+            'Kp_rudder' : 1.0,
+            'Kd_rudder' : 0.2,
         }
-        self.servo_lim = self.declare_parameter("servo_lim", default_params['servo_lim']).value
-        self.pwm_lim = self.declare_parameter("pwm_lim", default_params['pwm_lim']).value
+        self.rudder_lim = self.declare_parameter("rudder_lim", default_params['rudder_lim']).value
+        self.rudder_pwm_lim = self.declare_parameter("rudder_pwm_lim", default_params['rudder_pwm_lim']).value
+        self.thrust_pwm_lim = self.declare_parameter("thrust_pwm_lim", default_params['thrust_pwm_lim']).value
         self.dt = self.declare_parameter("dt", default_params['dt']).value
         self.Kp_thrust = self.declare_parameter("Kp_thrust", default_params['Kp_thrust']).value
         self.Kd_thrust = self.declare_parameter("Kd_thrust", default_params['Kd_thrust']).value
-        self.Kp_servo = self.declare_parameter("Kp_servo", default_params['Kp_servo']).value
-        self.Kd_servo = self.declare_parameter("Kd_servo", default_params['Kp_servo']).value
+        self.Kp_rudder = self.declare_parameter("Kp_rudder", default_params['Kp_rudder']).value
+        self.Kd_rudder = self.declare_parameter("Kd_rudder", default_params['Kd_rudder']).value
             
         namespace_OS = '/OS'
         
@@ -51,12 +53,12 @@ class PWMConverter(Node):
         self.err_spd = np.zeros(10)
         
         self.thrust_pwm = np.zeros(10)
-        self.servo_pwm = np.zeros(10)
+        self.rudder_pwm = np.zeros(10)
         
         self.OS_des_heading_sub = self.create_subscription(Float64, namespace_OS + '/des_heading', self.OS_des_heading_callback, 1)
         self.OS_des_spd_sub =self.create_subscription(Float64, namespace_OS + '/des_spd', self.OS_des_spd_callback, 1)
         self.OS_heading = self.create_subscription(Float64, namespace_OS + '/heading', self.OS_heading_callback, 1)
-        self.OS_spd = self.create_subscription(Float64, namespace_OS + '/spd', self.OS_des_spd_callback, 1)
+        self.OS_spd = self.create_subscription(Float64, namespace_OS + '/spd', self.OS_spd_callback, 1)
         
         self.pwm_pub =self.create_publisher(Int32, namespace_OS + '/pwm', 1)
         
@@ -93,6 +95,7 @@ class PWMConverter(Node):
         self.OS_des_spd_received = True
         self.OS_des_spd = np.append(self.OS_des_spd, msg.data)
         self.OS_des_spd = self.OS_des_spd[1:]
+        print(self.OS_des_spd[-1])
         
     def OS_heading_callback(self, msg):
         self.OS_heading_received = True
@@ -114,49 +117,50 @@ class PWMConverter(Node):
         self.OS_spd = self.OS_spd[1:]
         
         err_spd = self.OS_des_spd[-1] - self.OS_spd[-1]
+        # print(err_spd)
         self.err_spd = np.append(self.err_spd, err_spd)
         self.err_spd = self.err_spd[1:]
         
     def pub_pwm(self):
-        self.cal_thrust_pwm
-        self.cal_servo_pwm
+        self.cal_thrust_pwm()
+        self.cal_rudder_pwm()
 
-        # print(self.err_spd[-1])
-        # print(self.err_heading[-1])
-        # print(str(self.thrust_pwm[-1]))
-        # print(self.servo_pwm[-1])
+        thrust_pwm_str = str(int(self.thrust_pwm[-1])).zfill(4)
+        servo_pwm_str = str(int(self.rudder_pwm[-1])).zfill(4)
+        pwm_value = int(thrust_pwm_str + servo_pwm_str)
 
         pwm = Int32()
-
-        pwm.data = int(str(int(self.thrust_pwm[-1])) + str(int(self.servo_pwm[-1])))
-        
+        pwm.data = pwm_value
         self.pwm_pub.publish(pwm)
         
-    # functions that returns something
+    # functions that return something
     def cal_thrust_pwm(self):
-        thrust_pwm = self.Kp_thrust * self.err_spd[-1] - self.Kd_thrust * self.err_spd[-2] + (self.pwm_lim[0] + self.pwm_lim[1]) / 2
-        if thrust_pwm > self.pwm_lim[1]:
-            thrust_pwm = self.pwm_lim[1]
-        elif thrust_pwm < self.pwm_lim[0]:
-            thrust_pwm = self.pwm_lim[0]
+        thrust_pwm = self.Kp_thrust * self.err_spd[-1] - self.Kd_thrust * self.err_spd[-2] + \
+            (self.thrust_pwm_lim[0] + self.thrust_pwm_lim[1]) / 2
+        if thrust_pwm > self.thrust_pwm_lim[1]:
+            thrust_pwm = self.thrust_pwm_lim[1]
+        elif thrust_pwm < self.thrust_pwm_lim[0]:
+            thrust_pwm = self.thrust_pwm_lim[0]
+
+        # print(self.err_spd[-1], ":", self.err_spd[-2])
 
         thrust_pwm = int(thrust_pwm)
         
         self.thrust_pwm = np.append(self.thrust_pwm, thrust_pwm)
         self.thrust_pwm = self.thrust_pwm[1:]
     
-    def cal_servo_pwm(self):
-        servo = self.Kp_servo * self.err_heading[-1] - self.Kd_servo * self.err_heading[-2]
-        if servo > self.servo_lim[1]:
-            servo = self.servo_lim[1]
-        elif servo < self.servo_lim[0]:
-            servo = self.servo_lim[0]
-        norm_servo = (servo - self.servo_lim[0]) / (self.servo_lim[1] - self.servo_lim[0])
-        s_pwm = self.pwm_lim[0] + (self.pwm_lim[1] - self.pwm_lim[0]) * norm_servo
-        s_pwm = int(s_pwm)
+    def cal_rudder_pwm(self):
+        rudder = -(self.Kp_rudder * self.err_heading[-1] - self.Kd_rudder * self.err_heading[-2])
+        if rudder > self.rudder_lim[1]:
+            rudder = self.rudder_lim[1]
+        elif rudder < self.rudder_lim[0]:
+            rudder = self.rudder_lim[0]
+        norm_rudder = (rudder - self.rudder_lim[0]) / (self.rudder_lim[1] - self.rudder_lim[0])
+        r_pwm = self.rudder_pwm_lim[0] + (self.rudder_pwm_lim[1] - self.rudder_pwm_lim[0]) * norm_rudder
+        r_pwm = int(r_pwm)
         
-        self.servo_pwm = np.append(self.servo_pwm, s_pwm)
-        self.servo_pwm = self.servo_pwm[1:]
+        self.rudder_pwm = np.append(self.rudder_pwm, r_pwm)
+        self.rudder_pwm = self.rudder_pwm[1:]
     
 def main(args=None):
     rclpy.init(args=args)
